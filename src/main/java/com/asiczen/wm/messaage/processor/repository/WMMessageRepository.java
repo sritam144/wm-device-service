50f59bc4cf452cb5c4fd8faed460b80ce45c69d6
Sanjeet Mohanty <sanjeet215@gmail.com>
2021-02-20 12:31:28 +0530

    corrected all - test

--- src/main/java/com/asiczen/wm/messaage/processor/repository/WMMessageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.asiczen.wm.messaage.processor.repository;
-
-import com.asiczen.wm.messaage.processor.model.WMMessage;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface WMMessageRepository extends MongoRepository<WMMessage,String> {
-}
