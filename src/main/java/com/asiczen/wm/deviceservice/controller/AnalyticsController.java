eddd4160624657789d71b9b45793d56aff843a73
Sanjeet Mohanty <sanjeet215@gmail.com>
2021-02-20 22:03:47 +0530

    bug fix- controller

--- src/main/java/com/asiczen/wm/deviceservice/controller/AnalyticsController.java
+++ src/main/java/com/asiczen/wm/deviceservice/controller/AnalyticsController.java
@@ -5,10 +5,12 @@ import com.asiczen.wm.deviceservice.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController(value = "/customer")
+@RestController
+@RequestMapping(value = "/customer")
 public class AnalyticsController {
 
     @Autowired
